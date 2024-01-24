7d831a12e2e0ef00a2773d543a351af1da4e3df5
Cabal-98 <140062703+Cabal-98@users.noreply.github.com>
2024-01-24 18:24:50 +0100

    cell_noise_LIBRERIE.py

--- cell_noise_LIBRERIE.py
+++ cell_noise_LIBRERIE.py
@@ -3,6 +3,7 @@ import numpy.random as npr
 import matplotlib.pyplot as plt
 import math
 import random as rnd
+import scipy
 from scipy.special import factorial
 from scipy.integrate import odeint
 from scipy.special import comb
